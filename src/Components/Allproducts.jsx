8b60477702acc29d058b196b5feaf11b9528a9fe
הירשמן רחל לאה <  @mbjcomp.local>
2025-03-11 19:17:18 +0200

    change in  all products

--- src/Components/Allproducts.jsx
+++ src/Components/Allproducts.jsx
@@ -7,6 +7,7 @@ const AllProducts = ({ productsArr ,deleteProduct}) => {
     return (
 <div>
     <p>product data</p>
+    <h2>this is a beatiful data</h2>
     <ShowDetails productsArr={productsArr} deleteProduct={deleteProduct}/>
 </div>
     );
